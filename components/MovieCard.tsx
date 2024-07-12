d3c216e544c35e5d95627e151fb3c03907c61d80
Leandro Rey <leandrodrey@gmail.com>
2024-07-12 15:58:04 -0300

    Replace img tag with Next.js Image component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,13 +1,13 @@
 import {FC} from 'react';
 import Link from 'next/link';
+import Image from "next/image";
 import {IMovie} from '@/interfaces/IMovie';
 
 const Movie: FC<IMovie> = (movie) => {
     return (
         <Link href={`/MovieDetail/${movie.id}`}>
             <div className="movie bg-white-800 rounded p-4 transform hover:scale-105 transition duration-300 ease-in-out text-center">
-                <img
-                    src={movie.poster}
+                <Image  src={movie.poster}
                     alt={movie.title}
                     className="mb-2 rounded-md shadow-md transform hover:shadow-lg hover:scale-110 transition duration-300"
                 />
