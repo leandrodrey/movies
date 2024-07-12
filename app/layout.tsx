2cc8efeb300102c3e1e2b7f6b8ee4e8b83888eb3
Leandro Rey <leandrodrey@gmail.com>
2024-07-12 16:01:01 -0300

    Update import statement in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ProviderWrapper from "@/app/ProviderWrapper";
-import Header from "@/components/ui/header";
+import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
