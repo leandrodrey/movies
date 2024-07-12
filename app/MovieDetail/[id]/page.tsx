e87e1e4e8f4f752e6b74a4d576ee9ef9ee7f3732
Leandro Rey <leandrodrey@gmail.com>
2024-07-12 15:59:11 -0300

    Replace img tag with Next.js Image component
    This update replaces the standard HTML img tag with the Next.js Image component for better performance and optimization. Additionally, the code alignment was adjusted for better readability and consistency.

--- app/MovieDetail/[id]/page.tsx
+++ app/MovieDetail/[id]/page.tsx
@@ -1,4 +1,5 @@
 import {FC} from 'react';
+import Image from "next/image";
 import {IMovie} from "@/interfaces/IMovie";
 import {getMovieById} from "@/services/Movies";
 
@@ -17,16 +18,16 @@ const MovieDetailPage: FC<MovieDetailProps> = async ({params}) => {
         <div className="bg-black text-white p-8">
             <div className="flex flex-col md:flex-row">
                 <div className="md:w-1/3">
-                    <img src={movie.poster} alt={movie.title} className="w-full rounded-lg shadow-lg"/>
+                    <Image src={movie.poster} alt={movie.title} className="w-full rounded-lg shadow-lg"/>
                 </div>
                 <div className="md:w-2/3 md:pl-8 mt-4 md:mt-0">
                     <h1 className="text-4xl font-bold">{movie.title} ({movie.year})</h1>
                     <div className="flex items-center mt-2">
-            <span className="text-yellow-500">
-              {Array.from({length: Math.round(movie.rate || 0)}, (_, i) => (
-                  <i className="fas fa-star" key={i}></i>
-              ))}
-            </span>
+                        <span className="text-yellow-500">
+                            {Array.from({length: Math.round(movie.rate || 0)}, (_, i) => (
+                                <i className="fas fa-star" key={i}></i>
+                            ))}
+                        </span>
                         <span className="ml-2 text-gray-400">{movie.rate}</span>
                     </div>
                     <p className="mt-4 text-gray-400">{movie.genres}</p>
@@ -40,11 +41,11 @@ const MovieDetailPage: FC<MovieDetailProps> = async ({params}) => {
                         {movie.trailer && (
                             <a href={movie.trailer} target="_blank" rel="noopener noreferrer"
                                 className="bg-gradient-to-r from-red-500 to-red-700
-  text-white font-bold py-2 px-4 rounded
-  hover:from-red-600 hover:to-red-800
-  active:bg-red-900
-  focus:outline-none focus:ring focus:ring-red-300
-  transition duration-300 ease-in-out">
+                                text-white font-bold py-2 px-4 rounded
+                                hover:from-red-600 hover:to-red-800
+                                active:bg-red-900
+                                focus:outline-none focus:ring focus:ring-red-300
+                                transition duration-300 ease-in-out">
                                 Ver Trailer
                             </a>
                         )}
