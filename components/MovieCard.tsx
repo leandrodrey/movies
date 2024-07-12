4822074f467b888d811079c47bf54bd560add517
Leandro Rey <leandrodrey@gmail.com>
2024-07-12 18:43:26 -0300

    Update MovieCard component layout
    The dimensions of the movie poster image in the MovieCard component were updated from 200x300 to 300x450 to enhance visibility. Also, the movie title text alignment was updated to be centered for better layout presentation.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -8,12 +8,12 @@ const Movie: FC<IMovie> = (movie) => {
         <Link href={`/MovieDetail/${movie.id}`}>
             <div className="movie bg-white-800 rounded p-4 transform hover:scale-105 transition duration-300 ease-in-out text-center">
                 <Image  src={movie.poster}
-                    width={200}
-                    height={300}
+                    width={300}
+                    height={450}
                     alt={movie.title}
                     className="mb-2 rounded-md shadow-md transform hover:shadow-lg hover:scale-110 transition duration-300"
                 />
-                <h4 className="font-bold text-xs">{movie.title}</h4>
+                <h4 className="text-center font-bold text-xs">{movie.title}</h4>
             </div>
         </Link>
     );
