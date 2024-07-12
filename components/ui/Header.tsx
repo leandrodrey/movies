b757ecda910ab074b055c810d1a8dc03bdc9a149
Leandro Rey <leandrodrey@gmail.com>
2024-07-12 18:27:15 -0300

    Add logout functionality to Header component
    Updated the Header.tsx component to include a logout function. This function makes a POST request to the logout endpoint of the API and, if successful, the user is redirected to the home page and the local storage "user" record is removed. The display of the header was also modified to include a logout link when a user is logged in.

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,11 +1,30 @@
 'use client'
-
 import {FC, useContext} from 'react';
 import Link from 'next/link';
-import {UserContext} from '@/context/UserProvider'; // Assuming correct path
+import {UserContext} from '@/context/UserProvider';
+import {useRouter} from "next/navigation"; // Assuming correct path
 
 const Header: FC = () => {
-    const {user} = useContext(UserContext);
+    const {user, setUser} = useContext(UserContext);
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        try {
+            const logoutResponse = await fetch("https://codo-a-codo-js-api.vercel.app/auth/logout", {
+                method: "POST"
+            });
+
+            if (logoutResponse.ok) {
+                router.push("/");
+                localStorage.removeItem("user");
+                setUser(null);
+            } else {
+                console.error("Error al cerrar sesión:", logoutResponse.status);
+            }
+        } catch (error) {
+            console.error("Error al cerrar sesión:", error);
+        }
+    }
 
     // @ts-ignore
     return (
@@ -17,12 +36,17 @@ const Header: FC = () => {
 
                 {user ? (
                     <>
-                        <p>Bienvenido {user.user.name}!!</p>
-                        <Link href="/Admin/MovieAdmin">
-                            <button className="bg-red-600 text-white px-4 py-2 rounded">
+                        <div className="flex items-center">
+                            <p className="mr-1">Bienvenido {user.user.name}!!</p>
+                            |
+                            <Link className="ml-1 mr-1 text-blue-500 hover:text-blue-700" href="/Admin/MovieAdmin">
                                 Administrar películas
-                            </button>
-                        </Link>
+                            </Link>
+                            |
+                            <a href="#" onClick={handleLogout} className="ml-1 text-blue-500 hover:text-blue-700">
+                                Logout
+                            </a>
+                        </div>
                     </>
                 ) : (
                     <Link href="/Login">
